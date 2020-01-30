5c0f2e147e8bc320e8a687a156280eb0f1431415
Anwesh43 <anweshthecool0@gmail.com>
2020-01-30 11:13:53 +0530

    adding Canvas extension methods

--- jumpconsecutiveboxview/src/main/java/com/anwesh/uiprojects/jumpconsecutiveboxview/JumpConsecutiveBoxView.kt
+++ jumpconsecutiveboxview/src/main/java/com/anwesh/uiprojects/jumpconsecutiveboxview/JumpConsecutiveBoxView.kt
@@ -26,3 +26,28 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawJumpConsecutiveBox(i : Int, gap : Float, scale : Float, size : Float, h : Float, paint : Paint) {
+    val y : Float = (h / 2) * i
+    val hy : Float = (h / 2 - size / 2)
+    val sf : Float = scale.sinify().divideScale(i, boxes)
+    save()
+    translate(gap * i, y + hy * sf)
+    drawRect(RectF(-size / 2, -size / 2, size / 2, size / 2), paint)
+    restore()
+}
+
+fun Canvas.drawBoxes(gap : Float, scale : Float, size : Float, h : Float, paint : Paint) {
+    for (j in 0..(boxes - 1)) {
+        drawJumpConsecutiveBox(j, gap, scale, size, h, paint)
+    }
+}
+
+fun Canvas.drawJCBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    drawBoxes(gap, scale, size, h, paint)
+}
