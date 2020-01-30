c0240c8be1ec55b07553542019f00e58cabfe937
Anwesh43 <anweshthecool0@gmail.com>
2020-01-30 10:39:47 +0530

    adding constants and libraries

--- jumpconsecutiveboxview/src/main/java/com/anwesh/uiprojects/jumpconsecutiveboxview/JumpConsecutiveBoxView.kt
+++ jumpconsecutiveboxview/src/main/java/com/anwesh/uiprojects/jumpconsecutiveboxview/JumpConsecutiveBoxView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.jumpconsecutiveboxview
 /**
  * Created by anweshmishra on 30/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val boxes : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
